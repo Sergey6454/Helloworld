f6afecf1a91df455f6acbb4c44a83089c7dcab05
Sergey6454 <erga.baz@gmail.com>
2022-02-08 15:25:36 +0500

    first project

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World!");
+            Console.WriteLine("Oh, hi, Mark!");
+            Console.ReadKey();
+        }
+    }
+}
